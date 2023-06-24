50554204548d6652b5cc22929f9ced78325db8ec
Jakub Puszyński <jakub.puszynski@efitness.com>
2023-06-24 06:22:10 +0200

    Add project files.

---
+++ PlayerAccount/PlayerAccountModule.cs
@@ -0,0 +1,15 @@
+﻿namespace PlayerAccount
+{
+    public class PlayerAccountModule : IPlayerAccountModule
+    {
+        public void CreateAccount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogIn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
