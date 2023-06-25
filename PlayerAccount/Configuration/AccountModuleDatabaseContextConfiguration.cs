8f4e5d8585f3b2f2fca893a7fc193b690fa2902c
Jakub Puszyński <jakub.puszynski@efitness.com>
2023-06-25 20:55:28 +0200

    migration added

--- PlayerAccount/Configuration/AccountModuleDatabaseContextConfiguration.cs
+++ PlayerAccount/Configuration/AccountModuleDatabaseContextConfiguration.cs
@@ -1,16 +1,24 @@
-﻿// Ignore Spelling: Sql
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AccountModule.Configuration
 {
+    /// <summary>
+    /// to run migration
+    ///     cd.\PlayerAccount
+    ///     dotnet ef --startup-project ..\ProjectNomad\Server\ migrations add NameOfMigration
+    /// note: you are in migration and context library project and are referring to startup project
+    /// 
+    /// others commends:
+    ///     dotnet ef --startup-project ..\ProjectNomad\Server\ database update
+    /// </summary>
     public static class AccountModuleDatabaseContextConfiguration
     {
         public static void ConfigureSqlServerDataContext(IServiceCollection services, string connectionStrings)
         {
-            services.AddDbContextFactory<AccountContext>(options =>
-                options.UseSqlServer(connectionStrings));
+            services.AddDbContextFactory<AccountContext>(options => 
+                options.UseSqlServer(connectionStrings, 
+                x => x.MigrationsAssembly("AccountModule")));
         }
     }
 }
---
+++ PlayerAccount/Migrations/20230625183435_Init.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountModule.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GuestAccount_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GuestAccount_ReLoginToken = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegisteredAccount_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegisteredAccount_Password = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
