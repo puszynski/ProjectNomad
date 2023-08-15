4467c28b1a18fc6dc28a8002ff0b488b7f1d5d0f
Jakub Puszyński <puszynski@gmail.com>
2023-08-15 20:25:37 +0200

    init EventBroadcastService

---
+++ ProjectNomad/Client/Logic/EventBroadcastService.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectNomad.Client.Logic
+{
+    public class EventBroadcastService
+    {
+    }
+
+    public enum EActionWASM
+    {
+        None = 0,
+
+        NotificationAdded = 1,
+    }
+}
